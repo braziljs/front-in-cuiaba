b328d6ade08cf73b41dbe75044f47b79b5293849
gpedro <gpedro831@gmail.com>
2016-04-12 16:20:31 -0400

    feat(speakers, partner): add @castrolol, @joselitojunior1 and casa do código

--- docpad.js
+++ docpad.js
@@ -86,15 +86,9 @@ module.exports = {
         description: "Os portões se abrem para o alongamento"
     },
     {
-      name: "Abertura",
-      time: "08h10",
-      description: "... e o árbitro já está em campo...",
-    },
-    /*
-    Aguardando BIO
-    {
         name: "Bruno Pacola",
         photo: "themes/yellow-swan/img/eventhost-pacola.jpg",
+        bio: "Profissional Apaixonado pelo o que faz e privilegiado por ter descoberto muito cedo o que gosta de fazer na vida: trabalhar com tecnologia da informação. Nos dias de hoje, compartilha e constroi seus sonhos com pessoas fabulosas na empresa onde trabalha, Nuvem Tecnologia.",
         time: "08h10",
         type: "Apresentador",
         company: "Nuvem Tecnologia",
@@ -108,41 +102,10 @@ module.exports = {
           time: "08h10"
         }
     },
-    */
     {
         name: "Welcome Coffee",
         time: "08h20",
         description: "Foi dado início à partida!"
-    },
-    {
-        name: "Galvão Bueno",
-        time: "08h50",
-        description: "Bem, meus caros amigos, estamos aqui para dar início a mais uma partida ..."
-    },
-    {
-        name: "Rafael Venson",
-        photo: "themes/yellow-swan/img/speaker-rafael-venson.jpg",
-        bio: "Estudou Ciências Econômicas na Universidade do Estado do Mato Grosso (2006 a 2010) e formou em Gestão Comercial pela UNIRONDON (2012 a 2014), possui certificação em Master e Leader Coaching pela Sociedade Latino Americana de Coaching (SLAC). Durante cinco anos trabalhou no varejo do setor financeiro em três grandes instituições, Bradesco, HSBC e Santander, atuando como gerente de relacionamento em carteiras de alta renda. Em 2014 iniciou sua empresa de consultoria e treinamentos atendendo várias empresas no Mato Grosso com consultoria e treinamentos. Em 2015 ganhou participação na equipe da startup RCF Inovações na qual tem participação e atua como gestor.",
-        company: "Venson Consultoria",
-        link: {
-            href: "https://www.linkedin.com/in/rafaelvenson",
-            text: "@rafavenson"
-        },
-        presentation: {
-            title: "Criatividade não surge por acaso",
-            description: "Existe um mito de que a criatividade surge por acaso. Neste talk iremos mostrar que criatividade não é um mito e sim uma capacidade inata que precisa ser desenvolvida e cuja a utilidade é resolver problemas por meio da combinação de ideias e seguindo etapas de um processo criativo.",
-            time: "09h40"
-        }
-    },
-    {
-        name: "Vampeta",
-        time: "10h20",
-        description: "Manda nudez?!"
-    },
-    {
-        name: "Intervalo",
-        time: "11h00",
-        description: "Fim do primeiro tempo"
     },
     {
         name: "Gabriel Araujo",
@@ -156,8 +119,43 @@ module.exports = {
         presentation: {
             title: "Dialetos, Ferramentas e Interfaces. Uma breve historia da programação.",
             description: "De forma humorada contar rapidamente como as linguagens de programação acompanharam os paradigmas da computação, e repensar sobre para aonde estamos indo.",
-            time: "13h00"
+            time: "08h50"
         }
+    },
+    {
+        name: "Rafael Venson",
+        photo: "themes/yellow-swan/img/speaker-rafael-venson.jpg",
+        bio: "Estudou Ciências Econômicas na Universidade do Estado do Mato Grosso (2006 a 2010) e formou em Gestão Comercial pela UNIRONDON (2012 a 2014), possui certificação em Master e Leader Coaching pela Sociedade Latino Americana de Coaching (SLAC). Durante cinco anos trabalhou no varejo do setor financeiro em três grandes instituições, Bradesco, HSBC e Santander, atuando como gerente de relacionamento em carteiras de alta renda. Em 2014 iniciou sua empresa de consultoria e treinamentos atendendo várias empresas no Mato Grosso com consultoria e treinamentos. Em 2015 ganhou participação na equipe da startup RCF Inovações na qual tem participação e atua como gestor.",
+        company: "Venson Consultoria",
+        link: {
+            href: "https://www.linkedin.com/in/rafaelvenson",
+            text: "@rafavenson"
+        },
+        presentation: {
+            title: "Criatividade não surge por acaso",
+            description: "Existe um mito de que a criatividade surge por acaso. Neste talk iremos mostrar que criatividade não é um mito e sim uma capacidade inata que precisa ser desenvolvida e cuja a utilidade é resolver problemas por meio da combinação de ideias e seguindo etapas de um processo criativo.",
+            time: "09h50"
+        }
+    },
+    {
+        name: "Luan Castro",
+        photo: "themes/yellow-swan/img/speaker-castrolol.jpg",
+        bio: "Desenvolvedor inovador e apaixonado por natureza desde os 11 anos de idade, busca sempre estar por dentro das últimas tecnologias do mundo web, de mobile e de games. Desde 2009 aplicando Javascript para resolver todo e qualquer tipo de problema possível, atualmente trabalha no TJMT.",
+        company: "TJ/MT",
+        link: {
+            href: "https://www.twitter.com/castrolol",
+            text: "@castrolol"
+        },
+        presentation: {
+            title: "Você no controle, Service Workers (Serviços para Browser)",
+            description: "Primeiras experiencias e uso de Service Workers,  Push notifications no browser e usando cache para suporte offline, um passo largo nos seus poderes dentro do browser. The force (of service workers) may be with you.",
+            time: "10h50"
+        }
+    },
+    {
+        name: "Intervalo",
+        time: "11h50",
+        description: "Fim do primeiro tempo"
     },
     {
         name: "Juarez Filho",
@@ -171,7 +169,7 @@ module.exports = {
         presentation: {
             title: "Polymer & Firebase: Componentizando a Web em Tempo Real",
             description: "Polymer e Web Componentes trazem uma nova forma de construir aplicações Web. Nessa palestra você aprenderá como criar aplicações Web usando o Polymer Starter Kit e ainda armazenar os dados de sua aplicação, gerenciar autenticação do usuário e fazer deploy usando Firebase.",
-            time: "13h50"
+            time: "14h00"
         }
     },
     {
@@ -186,22 +184,32 @@ module.exports = {
         presentation: {
             title: "Desenvolvendo Aplicativos com React Native.",
             description: "React Native foca na eficiência do desenvolvedor entre as diferentes plataformas suportadas, aplicando o famoso 'Learn once, write anywhere'. O Facebook usa React Native em diversos aplicativos em produção e vai continuar investindo nesta tecnologia.",
-            time: "14h40"
+            time: "14h50"
         }
     },
     {
         name: "Intervalo",
-        time: "15h30",
+        time: "15h40",
         description: "Fim do segundo tempo"
     },
     {
-        name: "Pelé",
-        time: "16h00",
-        description: "Não, eu sou o Jô Soares..."
+        name: "Joselito Junior",
+        photo: "themes/yellow-swan/img/speaker-joselito-junior.jpg",
+        bio: "Organizador do GDG Recife e CodeTalks, co-criador e instrutor dos cursos AngularJS Bootcamp, PhoneGap Bootcamp e Ionic Bootcamp e fundador das startups premiadas Estudorama e Rabiscapp. Além de ganhador de Hackathons profissional, é entusiasta do Design Thinking, Lean Startup e desenvolvimento Ágil de software. Fã de Fórmula 1 e de fazer piadas de gosto duvidoso.",
+        company: "Tampa Works",
+        link: {
+            href: "https://twitter.com/joselitojunior1",
+            text: "@joselitojunior1"
+        },
+        presentation: {
+            title: "Progressive Web Apps: Um guia para você fazer webapps sem precisar de toalha",
+            description: "De tempos em tempos na web surgem novos paradigmas para a construção de aplicativos web. Aplicativos progressivos se aproveitam de novos conceitos e tecnologias, como App Manifest, ServiceWorkers, HTTPS, Offline e Mobile First, bem como conceitos de usabilidade e experiência do usuário. Progressivos, Responsivos, independentes, instaláveis... conheça todos esses conceitos e aprenda como construir o seu primeiro Progressive Web App enquanto almoça num restaurante no fim do universo.",
+            time: "16h10"
+        }
     },
     {
         name: "Mesa Redonda",
-        time: "16h50",
+        time: "17h00",
         description: "Fim do bate-bola, vamos ao terceiro tempo"
     },
     {
@@ -232,17 +240,17 @@ module.exports = {
     {
         name: 'Prata',
         items: [
+            {
+                name: "Código5",
+                logo: "themes/yellow-swan/img/sponsor-codigo5.png",
+                url: "http://codigo5.com.br"
+            }
         ]
     },
 
     {
         name: 'Bronze',
         items: [
-            {
-                name: "Código5",
-                logo: "themes/yellow-swan/img/sponsor-codigo5.png",
-                url: "http://codigo5.com.br"
-            }
         ]
     }
 
@@ -270,6 +278,11 @@ module.exports = {
             logo: "themes/yellow-swan/img/partner-yodojo.jpg",
             url: "https://www.facebook.com/yohacking"
         },
+        {
+            name: "Casa do Código",
+            logo: "themes/yellow-swan/img/partner-casa-do-codigo.png",
+            url: "https://www.casadocodigo.com.br"
+        }
     ],
 
     backstage: [
